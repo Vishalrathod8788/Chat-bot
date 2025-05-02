431a2465bbeab503c1fe3a8315f9bb773d4ceb06
Vishal Rathod <vishalrathod1562004@gmail.com>
2025-05-02 17:22:26 +0530

    chore: Add chatbot component and update UI

--- chat-bot/src/App.jsx
+++ chat-bot/src/App.jsx
@@ -3,6 +3,9 @@ import { Chatbot } from "./components/Chatbot";
 function app() {
   return (
     <div className="App">
+      <h2 style={{ textAlign: "center", marginTop: "20px" }}>
+        Customer Support Chatbot
+      </h2>
       <Chatbot />
     </div>
   );
--- chat-bot/src/components/Chatbot.jsx
+++ chat-bot/src/components/Chatbot.jsx
@@ -1,13 +1,39 @@
 import { Messages } from "./Messages";
+import { useState } from "react";
 import "./chatbot.css";
 
 export const Chatbot = () => {
+  const [message, setMessage] = useState("");
+  const [input, setInput] = useState("");
+
+  const handleSend = () => {
+    if (input.trim() === "") return;
+    const newMessage = {
+      Text: input,
+      isUser: true,
+    };
+
+    setMessage((prevMessages) => [...prevMessages, newMessage]);
+    setInput("");
+    // Simulate bot response
+    
+  };
   return (
-    <>
-      <h2 style={{ textAlign: "center" }}>Customer Support Chatbot</h2>
-      <Messages Text={"Hello, how can I help you?"} isUser={true} />
-      <Messages Text={"I need help with my order."} isUser={false} />
-    </>
+    <div className="chat-container">
+      <div className="chat-box">
+        {message.map((mes, index) => {
+          <Messages key={index} Text={mes.Text} isUser={mes.isUser} />;
+        })}
+      </div>
+      <div className="input-box">
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          onKeyDown={(e) => e.key === "Enter" && handleSend()}
+        />
+        <button onClick={handleSend}>Send</button>
+      </div>
+    </div>
   );
 };
-//chore: update dependencies
